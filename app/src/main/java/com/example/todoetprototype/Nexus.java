132ab132563607b43f794bf4b0287ef54e507f89
GameDeveloperEpsilon <ryanchapman2084@gmail.com>
2023-01-30 15:18:24 -0600

    Made changing activity tell which activity is being changed to.

--- app/src/main/java/com/example/todoetprototype/Nexus.java
+++ app/src/main/java/com/example/todoetprototype/Nexus.java
@@ -16,25 +16,25 @@ public class Nexus extends AppCompatActivity {
 
         Button petPageBtn = findViewById(R.id.petPageBtn);
         petPageBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Changing Activities", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Changing To Pet Activity", Toast.LENGTH_SHORT).show();
             Intent changeActivities = new Intent(this, PetActivity.class);
             startActivity(changeActivities);
         });
         Button plannerPageBtn = findViewById(R.id.plannerPageBtn);
         plannerPageBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Changing Activities", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Changing To Planner Activity", Toast.LENGTH_SHORT).show();
             Intent changeActivities = new Intent(this, PlannerActivity.class);
             startActivity(changeActivities);
         });
         Button inventoryPageBtn = findViewById(R.id.inventoryPageBtn);
         inventoryPageBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Changing Activities", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Changing To Inventory Activity", Toast.LENGTH_SHORT).show();
             Intent changeActivities = new Intent(this, InventoryActivity.class);
             startActivity(changeActivities);
         });
         Button storePageBtn = findViewById(R.id.storePageBtn);
         storePageBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Changing Activities", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Changing To Store Activity", Toast.LENGTH_SHORT).show();
             Intent changeActivities = new Intent(this, StoreActivity.class);
             startActivity(changeActivities);
         });
