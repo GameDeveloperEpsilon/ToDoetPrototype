d6b39acb5bfeb563f9584a551e3c8d82f288df53
GameDeveloperEpsilon <ryanchapman2084@gmail.com>
2023-02-07 17:07:15 -0600

    Added UI elements to TaskCreationActivity

--- app/src/main/java/com/example/todoetprototype/TaskCreationActivity.java
+++ app/src/main/java/com/example/todoetprototype/TaskCreationActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class TaskCreationActivity extends AppCompatActivity {
@@ -17,7 +20,19 @@ public class TaskCreationActivity extends AppCompatActivity {
         // Navigate to the Store page
         Button storePageBtn = findViewById(R.id.finishTaskCreation);
         storePageBtn.setOnClickListener(view -> {
-            Toast.makeText(this, "Finished Task Crration", Toast.LENGTH_SHORT).show();
+
+            // Get task name
+            EditText taskNameTextEdit = findViewById(R.id.etNewItem);
+            String taskName = String.valueOf(taskNameTextEdit.getText());
+
+            // Get selected difficulty
+            RadioGroup difficultySelector = findViewById(R.id.taskDifficultyRadioGroup);
+            int difficultyID = difficultySelector.getCheckedRadioButtonId();
+            RadioButton selectedDifficultyBtn = findViewById(difficultyID);
+            String taskDifficulty = (String) selectedDifficultyBtn.getText();
+
+            // Finish and switch activities
+            Toast.makeText(this, "Task Created: " + taskName + ", " + taskDifficulty, Toast.LENGTH_SHORT).show();
             Intent changeActivities = new Intent(this, PlannerActivity.class);
             startActivity(changeActivities);
         });
