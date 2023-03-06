3c79f2cfad442eb8573fdaaa7e7b0f0d3a1795ca
GameDeveloperEpsilon <ryanchapman2084@gmail.com>
2023-03-06 14:05:33 -0600

    Fixed store coin bug

--- app/src/main/java/com/example/todoetprototype/store/StoreActivity.java
+++ app/src/main/java/com/example/todoetprototype/store/StoreActivity.java
@@ -1,10 +1,13 @@
 package com.example.todoetprototype.store;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.todoetprototype.R;
+import com.example.todoetprototype.inventory.UserViewModel;
 
 public class StoreActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        UserViewModel userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+        userViewModel.getUserData().observe(this, user -> {
+
+            TextView usercoin = findViewById(R.id.usercoin);
+            usercoin.setText(String.valueOf(user.getCoins()));
+        });
     }
 }
