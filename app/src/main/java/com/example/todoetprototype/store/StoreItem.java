f984b635f97b7f732f10a85d5aa316eca7193e29
Iakruza <bluazu@gmail.com>
2023-03-06 18:35:17 -0600

    timer deletion

--- app/src/main/java/com/example/todoetprototype/store/StoreItem.java
+++ app/src/main/java/com/example/todoetprototype/store/StoreItem.java
@@ -1,4 +1,55 @@
 package com.example.todoetprototype.store;
 
-public class StoreItem {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreItem extends AppCompatActivity {
+
+    private int itemID;
+    private int itemprice;
+    private String itemname;
+    private String itemdescription;
+
+
+    public StoreItem(int itemID, int itemprice, String itemname, String itemdescription) {
+        this.itemID = itemID;
+        this.itemprice = itemprice;
+        this.itemname = itemname;
+        this.itemdescription = itemdescription;
+    }
+
+    //Getters and Setters
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public int getItemprice() {
+        return itemprice;
+    }
+
+    public void setItemprice(int itemprice) {
+        this.itemprice = itemprice;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public String getItemdescription() {
+        return itemdescription;
+    }
+
+    public void setItemdescription(String itemdescription) {
+        this.itemdescription = itemdescription;
+    }
+
+
 }
