184e324c9fe01a9e7c0140ed4f353e146a8f6537
Iakruza <bluazu@gmail.com>
2023-03-19 23:50:34 -0500

    Added death activity

---
+++ app/src/main/java/com/example/todoetprototype/pet/DeathActivity.java
@@ -0,0 +1,15 @@
+package com.example.todoetprototype.pet;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.todoetprototype.R;
+
+public class DeathActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_death);
+    }
+}
