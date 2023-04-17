72b577dd51a4f39def09ce9d34c7c4e17655bbf7
Iakruza <bluazu@gmail.com>
2023-04-17 14:33:42 -0500

    Cleaned up the code

--- app/src/main/java/com/example/todoetprototype/pet/PetModel.java
+++ app/src/main/java/com/example/todoetprototype/pet/PetModel.java
@@ -96,19 +96,7 @@ public class PetModel implements Serializable {
         BABY_SIORDON(2, "Siordon", 3, 1, R.drawable.baby_siordon_image),
         ADOLESCENT_SIORDON(3, "Siordon", 4, 2, R.drawable.adolescent_siordon_image),
         ADULT_SIORDON(4, "Siordon", 5, 3, R.drawable.axostageonetest),
-        ANCIENT_SIORDON(5, "Siordon", -1, 4, R.drawable.axostageonetest),
-
-        UNHATCH_KITVIX(6, "Kitvix", 7, -1, R.drawable.foxeggtest),
-        BABY_KITVIX(7, "Kitvix", 8, 6, R.drawable.foxspritetest),
-        ADOLESCENT_KITVIX(8, "Kitvix", 9, 7, R.drawable.axostageonetest),
-        ADULT_KITVIX(9, "Kitvix", 10, 8, R.drawable.axostageonetest),
-        ANCIENT_KITVIX(11, "Kitvix", -1, 9, R.drawable.axostageonetest),
-
-        UNHATCH_NYAKOLIS(12, "Nyakolis", 13, -1, R.drawable.categgtest),
-        BABY_NYAKOLIS(13, "Nyakolis", 14, 12, R.drawable.axostageonetest),
-        ADOLESCENT_NYAKOLIS(14, "Nyakolis", 15, 13, R.drawable.axostageonetest),
-        ADULT_NYAKOLIS(15, "Nyakolis", 16, 14, R.drawable.axostageonetest),
-        ANCIENT_NYAKOLIS(17, "Nyakolis", -1, 15, R.drawable.axostageonetest);
+        ANCIENT_SIORDON(5, "Siordon", -1, 4, R.drawable.axostageonetest);
 
 
         private final int drawable;
